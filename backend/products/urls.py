82fc315b849f8af23338fb7bab41cf96caaa9380
Gerald Lekara <117988041+gerald023@users.noreply.github.com>
2025-09-08 15:24:34 +0100

    changed the datatype for id and made gave cors access to the frontend of the application

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,12 +5,12 @@ from . import views;
 urlpatterns = [
     path("", views.product_list_create_view),
     path("create_product/", views.ProductCreateAPIView.as_view()),
-    path('<int:pk>/', views.ProductDetailsAPIViews.as_view()),
+    path('<uuid:pk>/', views.ProductDetailsAPIViews.as_view()),
     path('list_products/', views.ProductListAPIViews.as_view()),
     path('alt_view/', views.product_alt_view),
-    path('<int:pk>/method_details/', views.product_alt_view),
-    path('<int:pk>/update/', views.ProductUpdateAPIViews.as_view()),
-    path('<int:pk>/delete/', views.ProductDestroyAPIViews.as_view()),
+    path('<uuid:pk>/method_details/', views.product_alt_view),
+    path('<uuid:pk>/update/', views.ProductUpdateAPIViews.as_view()),
+    path('<uuid:pk>/delete/', views.ProductDestroyAPIViews.as_view()),
     path('mixin_view/', views.ProductMixinView.as_view()),
-    path('<int:pk>/mixin_details/', views.ProductMixinView.as_view()),
+    path('<uuid:pk>/mixin_details/', views.ProductMixinView.as_view()),
 ]
