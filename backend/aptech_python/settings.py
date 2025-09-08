7816fbbbbcad404889c222d059fd92f1a62b61d0
Gerald Lekara <117988041+gerald023@users.noreply.github.com>
2025-09-08 16:12:11 +0100

    changed the datatype for id and made gave cors access to the frontend of the application

--- backend/aptech_python/settings.py
+++ backend/aptech_python/settings.py
@@ -98,6 +98,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "https://online-food-ordering-system-if6f-bldj6oh8z.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 ROOT_URLCONF = 'aptech_python.urls'
 
